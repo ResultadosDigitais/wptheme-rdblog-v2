215cb8d0ca5eb7fcf1898fda1392ada88a59091d
liviamorim <liviavas.amorim@gmail.com>
2014-05-07 16:06:15 -0300

    :shit:

--- assets/functions/menus.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (function_exists('register_nav_menu'))
-{
-  register_nav_menu( 'header-menu', 'Cabeçalho' );
-  register_nav_menu( 'footer-menu', 'Rodapé' );
-}
-?>
--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-  <input type="search" class="search-field" placeholder="Pesquisar por..." value="" name="s" title="Search for:" />
-  <input type="submit" class="search-submit" value="Pesquisar" />
-</form>
