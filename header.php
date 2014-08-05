2f83e4a175056692bf9e41e882289efe094cc6e4
liviamorim <liviavas.amorim@gmail.com>
2014-08-05 09:47:56 -0300

    setup scopi blog

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
 
   <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
   <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'template_url' ); ?>/assets/css/font-awesome.min.css" />
+  <link href="http://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet" type="text/css" />
 
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery-min.js"></script>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/mexin.js"></script>
