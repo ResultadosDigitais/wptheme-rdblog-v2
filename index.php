8b0312feadad89ea6d710aa4cbf7d97b5846ffc1
liviamorim <liviavas.amorim@gmail.com>
2014-10-15 16:16:26 -0300

    Fix carousel conditional

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php get_header(); ?>
 
-<div id="carousel" class="owl-carousel">
-  <?php $featured_posts = new WP_Query( 'category_name=featured&posts_per_page=3' ); ?>
-  <?php while ( $featured_posts->have_posts() ) : $featured_posts->the_post(); ?>
-    <?php if (has_post_thumbnail( $post->ID ) ): ?>
-      <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-      <div class="slide" style="background-image:url('<?php echo $image[0]; ?>');">
-    <?php else : ?>
-      <div class="slide">
-    <?php endif; ?>
-      <div class="about">
-        <div class="container">
-          <h1><a href="<?php the_permalink(); ?>" alt="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
-          <a href="<?php the_permalink(); ?>" class="btn" alt="<?php the_title(); ?>">Saiba mais</a>
+<?php if ( is_home() ) { ?>
+  <div id="carousel" class="owl-carousel">
+    <?php $featured_posts = new WP_Query( 'category_name=featured&posts_per_page=3' ); ?>
+    <?php while ( $featured_posts->have_posts() ) : $featured_posts->the_post(); ?>
+      <?php if (has_post_thumbnail( $post->ID ) ): ?>
+        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+        <div class="slide" style="background-image:url('<?php echo $image[0]; ?>');">
+      <?php else : ?>
+        <div class="slide">
+      <?php endif; ?>
+        <div class="about">
+          <div class="container">
+            <h1><a href="<?php the_permalink(); ?>" alt="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
+            <a href="<?php the_permalink(); ?>" class="btn" alt="<?php the_title(); ?>">Saiba mais</a>
+          </div>
         </div>
       </div>
-    </div>
-  <?php endwhile; ?>
-</div>
+    <?php endwhile; ?>
+  </div>
+<?php } else {} ?>
 
 <div class="container">
   <div class="pure-g-r">
