9f2655335a2112d98854e3e63a645e6432ccab8c
Simone Beltrame <sibeltrame@gmail.com>
2015-06-12 09:54:35 -0300

    customize blog

--- assets/functions/options.php
+++ assets/functions/options.php
@@ -26,7 +26,7 @@ function theme_options_do_page() {
 
     <form method="post" action="options.php">
       <?php settings_fields( 'rd-mkt_options' ); ?>
-      <?php 
+      <?php
         $default_options = array(
           'header_desc' => '',
           'footer_desc' => '',
@@ -35,17 +35,17 @@ function theme_options_do_page() {
           'webprofile_facebook' => '',
           'webprofile_linkedin_id' => '',
           'webprofile_feedburner' => '',
-          'url_social_facebook' => '',
-          'url_social_twitter' => '',
+          'url_social_facebook' => 'http://facebook.com/',
+          'url_social_twitter' => 'http://twitter.com/',
           'url_social_gplus' => '',
-          'url_social_linkedin' => '',
+          'url_social_linkedin' => 'http://linkedin.com/in/',
           'url_social_youtube' => '',
           'url_social_flickr' => '',
           'url_social_pinterest' => '',
           'url_social_instagram' => '',
           'url_social_mail' => ''
         );
-        $options = get_option( 'rd-mkt_theme_options' ); 
+        $options = get_option( 'rd-mkt_theme_options' );
       ?>
 
       <table class="form-table">
@@ -92,6 +92,12 @@ function theme_options_do_page() {
         <tr valign="top"><th scope="row"><?php _e( 'LinkedIn - ID', 'wptheme-rdblog' ); ?></th>
           <td>
             <input id="rd-mkt_theme_options[webprofile_linkedin_id]" class="regular-text" type="text" name="rd-mkt_theme_options[webprofile_linkedin_id]" placeholder="<?php esc_attr_e( $default_options['webprofile_linkedin_id'] ); ?>" value="<?php esc_attr_e( ($options['options_edited'] != 'true') ? $default_options['webprofile_linkedin_id'] : $options['webprofile_linkedin_id'] ); ?>" />
+          </td>
+        </tr>
+
+        <tr valign="top"><th scope="row"><?php _e( 'Google Plus - ID', 'wptheme-rdblog' ); ?></th>
+          <td>
+            <input id="rd-mkt_theme_options[webprofile_gplus_id]" class="regular-text" type="text" name="rd-mkt_theme_options[webprofile_gplus_id]" placeholder="<?php esc_attr_e( $default_options['webprofile_gplus_id'] ); ?>" value="<?php esc_attr_e( ($options['options_edited'] != 'true') ? $default_options['webprofile_gplus_id'] : $options['webprofile_gplus_id'] ); ?>" />
           </td>
         </tr>
 
